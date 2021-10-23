39d176680a3f29e02a2a96cac6a55eee473212fe
Daniel Caaz <DCaazy@gmail.com>
2021-10-23 01:59:46 -0700

    Add a custom compare function to sort

--- vendor/sort.lua
+++ vendor/sort.lua
@@ -1,24 +1,29 @@
-function sort(a)
- function qpart(lo,hi)
-  local pivot=a[hi]
-  local i=lo
-  for j=lo,hi-1 do
-   if a[j]<=pivot then
-    a[i],a[j]=a[j],a[i]
-    i+=1
-   end
+-- todo: figure out where I found this.
+-- Been modified since then, to add a custom comparator.
+function sort(a, compare)
+  compare = compare and compare or function(a,b)
+    return a<=b
   end
-  a[i],a[hi]=a[hi],a[i]
-  return i
- end
- function qsort(lo,hi)
-  if lo<hi then
-   local p=qpart(lo,hi)
-   qsort(lo,p-1)
-   qsort(p+1,hi)
+  function qpart(lo,hi)
+    local pivot=a[hi]
+    local i=lo
+    for j=lo,hi-1 do
+      if compare(a[j], pivot) then
+        a[i],a[j]=a[j],a[i]
+        i+=1
+      end
+    end
+    a[i],a[hi]=a[hi],a[i]
+    return i
   end
- end
- qsort(1,#a)
- return a
+  function qsort(lo,hi)
+    if lo<hi then
+      local p=qpart(lo,hi)
+      qsort(lo,p-1)
+      qsort(p+1,hi)
+    end
+  end
+  qsort(1,#a)
+  return a
 end
 -- expect sort
