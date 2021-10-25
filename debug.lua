9e4bddfee1aa769bd7973c0c18ada3dc52799d04
Daniel Caaz <DCaazy@gmail.com>
2021-10-24 17:12:09 -0700

    add a depth limit to debug utility

--- debug.lua
+++ debug.lua
@@ -1,13 +1,18 @@
 -- require string_encode
 -- require binary/bits_from
 
-function debug(t,indent)
+function debug(t,depth,indent)
+  local depth = depth and depth or 2
   local tab = (indent and indent or '')
   local ty = type(t)
   if ty == 'table' then
     local s = ''
     for k,v in pairs(t) do
-      s = s..'\n'..tab..k..':'..debug(v,tab..' ')
+      if depth > 0 then
+        s = s..'\n'..tab..k..':'..debug(v,depth-1,tab..' ')
+      else
+        s = s..'\n'..tab..k..':MAXDEPTH'
+      end
     end
     return s
   elseif ty == 'string' or ty == 'number' then
