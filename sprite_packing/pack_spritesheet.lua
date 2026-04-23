a94a7fb1bf75f035cffb459e4745fbda5efd79bd
Daniel Cavazos <daniel@caaz.me>
2026-04-23 08:50:31 -0700

    cleanup

--- sprite_packing/pack_spritesheet.lua
+++ sprite_packing/pack_spritesheet.lua
@@ -3,10 +3,6 @@ function pack_spritesheet_1bit()
   for x = 0, 127 do
     for y = 0, size-1 do
       local bitfield = 0
-      -- bitfield += band(sget(x,y), 0b1)
-      -- bitfield += band(sget(x,y+size), 0b10)
-      -- bitfield += band(sget(x,y+size*2), 0b100)
-      -- bitfield += band(sget(x,y+size*3), 0b1000)
       bitfield += sget(x,y) != 0 and 0b1 or 0
       bitfield += sget(x,y+size) != 0 and 0b10 or 0
       bitfield += sget(x,y+size*2) != 0 and 0b100 or 0
